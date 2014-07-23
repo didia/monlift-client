e57189d29226a87c98ea2df98a5514997496adb2
Trispa <trispa88@gmail.com>
2014-07-23 17:45:06 -0400

    add myCars page

---
+++ www/js/app/src/components/myCars.js
@@ -0,0 +1,60 @@
+/** @jsx React.DOM */
+define(['jquery', 'react', 'app/monlift', 'app/auth', 'components/buttons'], function($, React, monlift, auth, buttons){
+
+	ML = monlift.getInstance();
+	return{
+	MyCars :React.createClass({displayName:'myCars',
+	render:function(){
+		
+		return(
+			<div className = "myCarscomponent" id = "myCarscomponent" >
+							
+								<ul className="table-view">
+								  <li className="table-view-cell media">
+									<a className="navigate-right">
+									  <img className="media-object pull-left" src="../img/car2.png"  height="80" weight="80"/>
+									  <div className="media-body">
+										voiture 1
+										<p>Marque voiture
+										infos sur voiture</p>
+									  </div>
+									</a>
+								  </li>
+								 
+							
+								  <li className="table-view-cell media">
+									<a className="navigate-right">
+									  <img className="media-object pull-left" src="../img/car2.png"  height="80" weight="80"/>
+									  <div className="media-body">
+										Voiture 2
+										<p>renauld Megane with .... or Similar
+										compact Manual</p>
+									  </div>
+									</a>
+								  </li>
+								  
+								  <li className="table-view-cell media">
+									<a className="navigate-right">
+									  <img className="media-object pull-left" src="../img/car2.png"  height="80" weight="80" />
+									  <div className="media-body">
+										Voiture 3
+										<p>renauld Megane with .... or Similar
+										compact Manuals</p>
+									  </div>
+									</a>
+								  </li>
+								</ul>
+			</div>
+									
+								  
+							
+			  
+
+                   );
+					
+				}
+			})
+		}
+})
+
+
