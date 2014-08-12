0f78c45368235dc7fb6f3c58163793695bf545d6
Trispa <trispa88@gmail.com>
2014-08-12 18:18:11 -0400

    apdate

---
+++ www/js/app/src/components/car.js
@@ -0,0 +1,28 @@
+/** @jsx React.DOM */
+define(['jquery', 'react', 'app/monlift'], function($, React, monlift){
+	
+	ML  = monlift.getInstance();
+	var Car = React.createClass({displayName:'Lift', 
+		render: function(){
+			return (
+								<li className="table-view-cell media">
+             
+									<a className="navigate-right">
+									  <img className="media-object pull-left" src="../img/car2.png"  height="80" weight="80"/>
+									  <div className="media-body">
+										{this.props.nom}
+										<p>{this.props.matricule}
+										this.props.description</p>
+									  </div>
+									</a>
+								 </li>
+                       
+
+
+			)
+		}
+	});
+	
+	return Car;
+	
+});// JavaScript Document
