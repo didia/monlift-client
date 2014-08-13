cc7b19c1c40e91f86f4ae868a6db24f3f6da2dc3
Trispa <trispa88@gmail.com>
2014-08-13 02:10:36 -0400

    delete file

---
+++ www/js/app/src/entities/lift.js
@@ -0,0 +1,53 @@
+ /**
+ * @author Trispa
+ *
+ */
+ define(function(){
+ function Lift(p_from, p_to, p_time, p_meetingPlace, p_totalPlace, p_availablePlace, p_driver, p_car)
+ {
+	if(!(this instanceof Lift)){
+		throw new typeError("Lift constructor conot be called as a function.");
+	}
+		this.m_from = p_from;
+		this.m_to = p_to;
+		this.m_time = p_time;
+		this.m_meetingPLace = p_meetingPlace;
+		this.m_totalPlace = p_totalPlace;
+		this.m_availablePLace = p_availablePlace;
+		this.m_driver = p_driver;
+		this.m_car = p_car;
+ }
+ 
+ Lift.prototype = {
+	 constructor: Lift,
+	 
+	 getFrom:function(){
+		 return this. this.m_from ;
+	 },
+	 getTo:function(){
+		 return this.m_to;
+	 },
+	 gettime:function(){
+	 	return this.m_time;
+	 },
+	 getMeetingPLace:function(){
+		 return this.m_meetingPlace;
+	 },
+	 getTotalPlace:function(){
+		 return this.m_totalPlace;
+	 },
+	 getAvailablePLace:function(){
+		 return this.m_availablePLace;
+	 },
+	 getDriver:function(){
+		 return this.m_driver;
+	 },
+	 getCar:function(){
+		 return this.m_car;
+	 }
+ 
+ };
+ return Lift;
+ });
+// JavaScript Document
+// JavaScript Document
