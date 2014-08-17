fca1a85df08ffff8bad1822bfbcbd1cc620e0b16
GeekInside <abdou_oi@yahoo.fr>
2014-08-17 19:15:56 -0400

    matricule min and max length verification

--- www/js/app/src/entities/car.js
+++ www/js/app/src/entities/car.js
@@ -9,8 +9,15 @@
 	if(!(this instanceof Car)){
 		throw new typeError("Car constructor conot be called as a function.");
 	}
-		 thia.nom = p_nom
-		 this.matricule = p_matricule ;
+		 this.nom = p_nom
+                       if(p_matricule.length >= 5 && p_matricule.length <= 7)
+		 {
+                            this.matricule = p_matricule ;
+                       }
+                       else
+                       {
+                            this.matricule = " ";
+                       }
 		 this.description = p_description;
 		 this.owner = p_owner;
  }
@@ -22,7 +29,7 @@
 		 return this.nom;
 	 },
 	 getMatricule:function(){
-		 return this. this.matricule ;
+		 return this.matricule ;
 	 },
 	 getDescription:function(){
 		 return this.description;
