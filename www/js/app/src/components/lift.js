34e509ca03b5f042206132bfc996d0a85049f1bd
Trispa <trispa88@gmail.com>
2014-08-18 07:04:44 -0400

    display cars liste and lift list like a card

--- www/js/app/src/components/lift.js
+++ www/js/app/src/components/lift.js
@@ -11,20 +11,20 @@ define(['jquery', 'react', 'app/monlift'], function($, React, monlift){
 			
 			return (               
                
-                        
-                           <li className="table-view-cell media">
+			   				
+                        	<div className="card">
 								<a className="navigate-right">
+                        
+                           	
                            		<img className="media-object pull-left" src ={this.props.image} height="70" 	weight="70" />
 
-                           		<div className = "vertical-centered">	
-								 {this.props.date}
+                           		 {this.props.date}
 								  <p>De {" "+ this.props.depart} vers{" "+ this.props.arrivee +" "} au prix de {" "+this.props.prix +"."} Conducteur : {this.props.driver}</p>  	
 								  
 								 <p>{this.props.placesdisponible} </p>
-								 
-								</div>
-								</a>
-							</li>
+								</a>	 
+							</div>
+							
 						
                        
 
