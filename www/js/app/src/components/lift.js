d51c25f9bf1f3d00eb4c96b1fe7d012a64a3be10
Trispa <trispa88@gmail.com>
2014-08-25 09:28:32 -0400

    reorganiser l'Affichage de la liste des lifts comme carte

--- www/js/app/src/components/lift.js
+++ www/js/app/src/components/lift.js
@@ -9,28 +9,26 @@ define(['jquery', 'react', 'app/monlift'], function($, React, monlift){
 	var Lift = React.createClass({displayName:'Lift', 
 		render: function(){
 			
-			return (               
-              			
-                        	<div className="card">
-								<a className="navigate-right" href ="#">
-                        
-                           	
-                           		<img className="media-object pull-left" src ={this.props.image} height="70" 	weight="70" />
-
-                           		 {this.props.date}
-								  <p>De {" "+ this.props.depart} vers{" "+ this.props.arrivee +" "} au prix de {" "+this.props.prix +"."} Conducteur : {this.props.driver} Nombre de place : {this.props.placesdisponible} </p>
-								</a>	 
-							</div>
-						
+			return (
+				<div className="card">
 					
-							
-							
-							
+  					<li className="table-view-cell media">
+ 
+						<a className="navigate-right">
 						
-                       
-
-
-			)
+						  <img className="media-object pull-left" src ={this.props.image} height="70" 	weight="70" />
+							<div className="media-body">
+							
+							{this.props.date}
+								<p>De {" "+ this.props.depart} vers{" "+ this.props.arrivee +" "} au prix de {" "+this.props.prix +"."} Conducteur : {this.props.driver} Nombre de place : {this.props.placesdisponible} </p>
+						  
+						
+						 </div>
+						 </a>
+					</li>
+					
+				</div>
+		)
 		}
 	});
 	
